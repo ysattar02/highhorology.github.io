0912bdfb317cdfc69d08d1c4ff299ef85719ded5
ysattar02 <yusufsattar@gmail.com>
2024-09-08 23:40:46 -0400

    updates

---
+++ js/about_me_scripts.js
@@ -0,0 +1,4 @@
+//function to take you back to the homepage
+function about_me_goToHomepage(){
+    window.location.href = '../index.html';
+}
---
+++ js/index_scripts.js
@@ -0,0 +1,8 @@
+//function to take you to about me page
+function index_goToAboutMe(){
+    window.location.href = "html/about_me.html";
+}
+
+function index_goToSOTC(){
+    window.location.href = "html/SOTC.html";
+}
